b84ff84658a27baaed88ac4911ed1f9e7388d917
ronny141 <rcontreraz20@gmail.com>
2024-09-12 19:14:06 -0500

    agregar a PopusProvider a proyecto 1

--- apps/proyecto-1/src/app/app.tsx
+++ apps/proyecto-1/src/app/app.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Dashboard } from '../screens';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="RROOONNN" />
+      <Dashboard />
     </div>
   );
 }
---
+++ apps/proyecto-1/src/screens/Dashboard.tsx
@@ -0,0 +1,49 @@
+import { PopupsProvider, usePopups } from '@monitoreo-inteligente/proyecto-2';
+export function Dashboard() {
+  const { addPopup } = usePopups();
+
+  const handleAddPopupA = () => {
+    const id = `popup-a-${Date.now()}`;
+    addPopup({
+      id,
+      title: `Popup A`,
+      left: 100,
+      top: 100,
+      width: 300,
+      height: 200,
+      zIndex: Date.now(),
+    });
+  };
+
+  const handleAddPopupB = () => {
+    const id = `popup-b-${Date.now()}`;
+    addPopup({
+      id,
+      title: `Popup B`,
+      left: 300,
+      top: 300,
+      width: 400,
+      height: 250,
+      zIndex: Date.now(),
+    });
+  };
+  return (
+    <div>
+      <h1>Ronny Contreras</h1>
+      <div
+        style={{ width: '700px', height: '700px', border: '1px solid black' }}
+      >
+        <PopupsProvider>
+          <button onClick={handleAddPopupA} style={{ margin: '10px' }}>
+            Agregar Popup A
+          </button>
+          <button onClick={handleAddPopupB} style={{ margin: '10px' }}>
+            Agregar Popup B
+          </button>
+        </PopupsProvider>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
